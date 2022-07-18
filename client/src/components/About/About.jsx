6f4522b583432231d758086ac3af238361ea71de
Daniel Bae <danibae91@icloud.com>
2022-07-18 13:14:15 -0700

    update Hero, About and Navbar components with dark background and style

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -3,22 +3,34 @@ import "./About.scss";
 const About = () => {
     return (
         <div id="about" className="about">
-            <div className="about-headline">
-                <div className="highlight"></div>
-                <h2>Frontend developer<span>.</span></h2>
-            </div>
-            <div className="about-introduction">
-                <p>I have two years of hands-on experience creating interactive and user-friendly applications, using the latest technologies.</p>
-            </div>
-            <a href="https://drive.google.com/file/d/1wucwIMj5H4cytQp3QRCMEblhZZR_8Y3k/view?usp=sharing" target="_blank" rel="noreferrer" className="btn">See Resume</a>
-            <div className="about-life">
-                <div className="about-life--left">
-                    <p>My journey in web development started back in 2020 when I wrote my first line of code, <span>Hello World</span>. This simple, yet powerful line was the beginning of it all.</p>
-                    <p>HTML, CSS and JavaScript gave me the elementary tools to start building creative and intuitive websites.</p>
-                    <p>Fast-forward to today, most of my attention in web development goes to React.js. combined with Node.js, Express.js and a mixture of PostgreSQL and MongoDB. Creating clean, functional, scalable, and testable applications are my main focus.</p>    
+            <div className="about-wrapper">
+                <div className="about-headline">
+                    <h2>Frontend developer<span>.</span></h2>
                 </div>
-                <div className="about-life--right">
-                    <img src="https://avatars.githubusercontent.com/u/63386733?v=4" alt="github-avatar"/>
+                <div className="about-introduction">
+                    <p>I have two years of hands-on experience creating interactive and user-friendly applications, using the latest technologies.</p>
+                </div>
+                <a href="https://drive.google.com/file/d/1wucwIMj5H4cytQp3QRCMEblhZZR_8Y3k/view?usp=sharing" target="_blank" rel="noreferrer" className="btn">See Resume</a>
+                <div className="about-life">
+                    <div className="left">
+                        <p>My journey in web development started back in 2020 when I wrote my first line of code, <span>Hello World</span>. This simple, yet powerful line was the beginning of it all.</p>
+                        <p>HTML, CSS and JavaScript gave me the elementary tools to start building creative and intuitive websites.</p>
+                        <p>Fast-forward to today, most of my attention in web development goes to React.js. combined with Node.js, Express.js and a mixture of PostgreSQL and MongoDB. Creating clean, functional, scalable, and testable applications are my main focus.</p>                        
+                    </div>
+                    <div className="right">
+                        <button>
+                            JavaScript (ES6+)
+                        </button>
+                        <button>
+                            React.js
+                        </button>
+                        <button>
+                            Node.js
+                        </button>
+                        <button>
+                            PostgreSQL/MongoDB
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,18 +1,19 @@
-import ArrowDropDownCircleIcon from '@mui/icons-material/ArrowDropDownCircle';import "./Hero.scss"; 
+import profile from "../../constants/profile.JPG"; 
+import "./Hero.scss"; 
 
 const Hero = () => {
     return (
         <div id="hero" className="hero">
-            <div className="hero-headline">
-                <p>Hi There, I'm <span>Danny.</span></p>
-                <h2>Frontend Developer<span>.</span></h2>
-            </div>
-            <div className="hero-introduction">
-                <p>I'm passionate about finding ways to efficiently solve problems and make a positive difference for today and for tomorrow.</p>
-            </div>
-            <a href="#about" className="btn">About Me</a>
-            <div className="hero-svg">
-                <a href="#about"><ArrowDropDownCircleIcon /></a>
+            <div className="hero-wrapper">
+                <img src={profile} alt="avatar" />
+                <div className="hero-headline">
+                    <p>Hi I'm Danny Bae</p>
+                    <h2>Frontend Developer<span>.</span></h2>
+                </div>
+                <div className="hero-introduction">
+                    <p>I'm passionate about finding ways to efficiently solve problems and make a positive difference for today and for tomorrow.</p>
+                </div>
+                <a href="#about" className="btn">About Me</a>
             </div>
         </div>
     )
