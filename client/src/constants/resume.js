4627edaa57dc1eca10ac36344a5ec61efb508cfb
Daniel Bae <danibae91@icloud.com>
2022-09-27 15:13:01 -0700

    update resume link

--- client/src/constants/resume.js
+++ client/src/constants/resume.js
@@ -4,6 +4,6 @@
 export const resumeLink = [
     {
         id: 1, 
-        url: "https://drive.google.com/file/d/1YHCZHocEyGPSRRQACtpXqKXpxOFfc1Ao/view?usp=sharing"
+        url: "https://drive.google.com/file/d/1rzAwaL2VnlZ0h_2DsqV9jnjJREGaycxI/view?usp=sharing"
     }
 ]; 
