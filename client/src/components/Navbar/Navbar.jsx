0e109fa0eb2a3dbd7e2363160d1021ff7de69871
Daniel Bae <danibae91@icloud.com>
2023-03-19 11:15:48 -0500

    update Navbar for window resizing

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,25 @@
+import { useEffect } from "react";
 import { links } from "../../constants/links"; 
 import { resumeLink } from "../../constants/resume";
 import "./Navbar.scss"; 
 
 const Navbar = ({ menuOpen, setMenuOpen }) => {
+    // useEffect 
+    // when resizing window we'll handle state back to closing mobileMenu/navbar bg
+    useEffect(() => {
+        const handleResize = () => {
+            if(menuOpen) {
+                setMenuOpen(!menuOpen); 
+            };
+        };
+
+        window.addEventListener("resize", handleResize); 
+
+        return () => {
+            window.removeEventListener("resize", handleResize); 
+        };
+    }, [menuOpen, setMenuOpen]);
+
     return (
         <div className={"navbar " + (menuOpen && "active")}>
             <div className="navbar-left">
