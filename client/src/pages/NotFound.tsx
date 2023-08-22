dccabe511fbacd9fb5c409dc6946c35035000320
Daniel Bae <danibae91@icloud.com>
2023-08-22 18:31:27 -0500

    minimum styling for notfound page

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,7 +1,23 @@
+import { Button } from "../components";
+import "../styles/NotFound.css"; 
+
 export default function NotFound() {
   return (
-    <div>
-      <h1>Not Found. Please go back</h1>
+    <div className="notFound">
+      <div className="notFound__wrapper">
+        <section className="notFound__main">
+          <div className="notFound__title">
+            <h1>Not Found<span className="dot--green">...</span> 🚒</h1>
+          </div>
+          <div className="breakline"></div>
+          <div className="notFound__intro">
+            <p>404 | Looks like you encountered an error</p>
+          </div>
+        </section>
+        <section className="notFound__bottom">
+          <Button title="back to safety" url="/" />
+        </section>
+      </div>
     </div>
   )
 }
