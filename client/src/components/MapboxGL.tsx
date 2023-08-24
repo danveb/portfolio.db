015d5d5c2344d4b8b98988af1b516549bfa1e3e0
Daniel Bae <danibae91@icloud.com>
2023-08-24 16:34:52 -0500

    try a fix for MapboxGL component on production

--- client/src/components/MapboxGL.tsx
+++ client/src/components/MapboxGL.tsx
@@ -1,4 +1,5 @@
-import React, { useRef, useState, useEffect } from "react"; 
+import { useRef, useState, useEffect } from "react"; 
+// @ts-ignore
 import mapboxgl, { Map } from "mapbox-gl" // eslint-disable-line import/no-webpack-loader-syntax
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API || ""; // added fallback for accessToken
@@ -14,19 +15,7 @@ export default function MapboxGL() {
   // useEffect
   useEffect(() => {
     if(map.current) return; // initialize map just once
-    // map.current = new mapboxgl.Map({
-    //   container: mapContainer.current!,
-    //   style: "mapbox://styles/mapbox/dark-v11", 
-    //   center: [lng, lat], 
-    //   zoom: zoom, 
-    // }); 
-
-    // if(!map.current) return; // wait for map to initialize 
-    // map.current.on("move", () => {
-    //   setLng(map.current.getCenter().lng.toFixed(4));
-    //   setLat(map.current.getCenter().lat.toFixed(4)); 
-    //   setZoom(map.current.getZoom().toFixed(2)); 
-    // });
+    
 
     const mapOptions: mapboxgl.MapboxOptions = {
       container: mapContainer.current!, 
