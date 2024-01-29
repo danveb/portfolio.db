c941ff1b445e42c3315ea562321af7a046accefd
Daniel Bae <danibae91@icloud.com>
2024-01-29 15:17:09 -0600

    minimal update to home page/style

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,5 @@
-import { Button, Gallery } from "../components"; 
-import "../styles/Home.css"; 
+import { Button, Gallery } from "../components";
+import "../styles/Home.css";
 
 export default function Home() {
   return (
@@ -12,11 +12,7 @@ export default function Home() {
           <div className="breakline"></div>
           <div className="home__intro">
             <p>I'm a <span>frontend developer</span>, passionate about finding ways to efficiently solve complex problems and make a positive difference for today and for tomorrow. I'm currently building my personal portfolio, which contains some of my latest projects. </p>
-          </div>
-          <div className="home__intro">
             <p>Over the past couple of years, I've dived into the technologies I'm interested in at the time, and how I learned to grow in the development world. </p>
-          </div>
-          <div className="home__intro">
             <p>Feel free to continue browsing my portfolio to know a little more about me, and the type of projects I love to create. </p>
           </div>
           <Button title="read my story" url="/about" />
